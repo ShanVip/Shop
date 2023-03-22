770a122427bfe3ead85f33ae62c1d6065da9c871
Shan <shanpiv@gmail.com>
2023-03-22 20:30:12 +0300

    ShopSping

---
+++ src/main/java/com/program/shop/controllers/MainController.java
@@ -0,0 +1,31 @@
+package com.program.shop.controllers;
+
+
+import com.program.shop.modeles.Item;
+import com.program.shop.repo.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @GetMapping("/")
+    public String index(Model model){
+        Iterable<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(@RequestParam(name = "userName", required = false, defaultValue = "World") String userName, Model model){
+        model.addAttribute("name", userName);
+        return "about";
+    }
+
+}
---
+++ src/main/java/com/program/shop/modeles/Item.java
@@ -0,0 +1,58 @@
+package com.program.shop.modeles;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title, info, image;
+
+    private int price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
